ea6bf7e4ffce87a0e7cf56917d7da9235a1ebaf9
SubasishMohapatra <subasish.mohapatra@daimler.com>
2018-10-28 03:40:13 +0530

    Squirel Demo App basic version done

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Squirrel;
 
 namespace SquirrelDemo
 {
@@ -13,5 +14,35 @@ namespace SquirrelDemo
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var updateManager = new UpdateManager(@"C:\Users\SUMOHAP\SquirrelDemo");
+            SquirrelAwareApp.HandleEvents(
+                onInitialInstall: v =>
+                {
+                    updateManager.CreateShortcutForThisExe();
+                    updateManager.CreateUninstallerRegistryEntry();
+                },
+                onAppUpdate: v =>
+                {
+                    //updateManager.RemoveShortcutForThisExe();
+                    updateManager.CreateShortcutForThisExe();
+                    //updateManager.RemoveUninstallerRegistryEntry();
+                    updateManager.CreateUninstallerRegistryEntry();
+                    //UpdateManager.RestartApp();
+                },
+                onAppUninstall: v =>
+                {
+                    updateManager.RemoveShortcutForThisExe();
+                    updateManager.RemoveUninstallerRegistryEntry();
+                });
+            //onFirstRun: () => ShowTheWelcomeWizard = true);
+            // Create the startup window
+            MainWindow wnd = new MainWindow();
+            // Do stuff here, e.g. to the window
+            wnd.Title = "Squirrel Demo";
+            // Show the window
+            wnd.Show();
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,7 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Squirrel;
 
 namespace SquirrelDemo
 {
@@ -23,6 +26,48 @@ namespace SquirrelDemo
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
         }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Hello");
+            //AppVersionInfo = $"Current version: {System.Reflection.Assembly.GetEntryAssembly().GetName().Version}";
+            bool restart = false;
+            var appPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\SquirrelDemo\\SquirrelDemo.exe";
+            using (var updateManager = new UpdateManager(@"C:\Users\SUMOHAP\SquirrelDemo"))
+            {
+                //AppVersionInfo = $"Current version: {updateManager.CurrentlyInstalledVersion()}";
+                var appUpdateInfo = await updateManager.CheckForUpdate();
+                if (appUpdateInfo != null && appUpdateInfo.ReleasesToApply.Any())
+                {
+                    var newVersion = appUpdateInfo.ReleasesToApply[appUpdateInfo.ReleasesToApply.Count - 1];
+                    var msgBoxresult = MessageBox.Show($"Update Version: {newVersion.Version}. Press ok to update.", "New version detected", MessageBoxButton.YesNo);
+                    if (msgBoxresult == MessageBoxResult.Yes)
+                    {
+                        var releaseEntry = await updateManager.UpdateApp();
+                        MessageBox.Show($"App upgraded to: {releaseEntry?.Version.ToString() ?? "No update"}" +
+                                        $"{Environment.NewLine}" +
+                                        $"Path={appPath}", "New app info");
+                        //updateManager.RemoveUninstallerRegistryEntry();
+                        //await updateManager.CreateUninstallerRegistryEntry();
+                        //Process.Start(newExePath);
+                        //await Task.Run(()=>UpdateManager.RestartApp());
+                        restart = true;
+                    }
+                }
+            }
+            if (restart)
+            {
+                Process.Start(appPath);
+                Application.Current.Shutdown(0);
+            }
+            //UpdateManager.RestartApp();
+        }
+
+        public string AppVersionInfo => $"s/w version: {System.Reflection.Assembly.GetEntryAssembly().GetName().Version}";
+        //public string AppVersionInfo { get; set; }
+
+        public string AppLocation => $"{System.Reflection.Assembly.GetEntryAssembly().Location}";
     }
 }
